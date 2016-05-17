f7b505eea816d88b067d0fe741ca8710d93ff4e2
silverwind <me@silverwind.io>
2016-05-17 19:43:15 +0200

    refactor, add process title

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 "use strict";
 
-const fs = require("fs");
+process.title = "pagediff";
+
 const join = require("path").join;
 const express = require("express");
 const app = express();
@@ -11,6 +12,7 @@ const slugify = require("slugify-url");
 
 const client = join(__dirname, "client");
 const img = join(__dirname, "img");
+const port = process.env.PORT || 4000;
 
 function imgPath(name) { return join(img, slugify(name)) + ".png"; }
 function imgUrl(name) { return "img/" + slugify(name) + ".png"; }
@@ -19,46 +21,41 @@ app.use(require("body-parser").json());
 app.use("/", express.static(client, {maxAge: 0}));
 app.use("/img", express.static(img, {maxAge: 0}));
 
-app.post("/diff", function(req, res) {
-  var a = req.body.a, b = req.body.b;
-  var dims = req.body.w + "x" + req.body.h;
+app.post("/diff", (req, res) => {
+  const a = req.body.a, b = req.body.b, dims = req.body.w + "x" + req.body.h;
   new Pageres({delay: 1, crop: false, filename: "<%= url %>"})
-  .src(a, [dims]).src(b, [dims]).dest(img).run()
-  .then(function() {
-    getDiff(a, b).then(function(perc) {
-      res.json({
-        err: null,
-        a: imgUrl(a),
-        b: imgUrl(b),
-        diff: imgUrl(a + "-" + b),
-        perc: perc,
-      });
+  .src(a, [dims]).src(b, [dims]).dest(img).run().then(() => getDiff(a, b)).then(perc => {
+    res.json({
+      a: imgUrl(a),
+      b: imgUrl(b),
+      diff: imgUrl(a + "-" + b),
+      perc: perc,
     });
-  }).catch(function(err) {
+  }).catch(err => {
     console.error(err);
     res.json({err: err});
   });
 });
 
-const port = process.env.PORT || 4000;
-app.listen(port, "0.0.0.0", function() {
+app.listen(port, "0.0.0.0", () => {
   console.log("pagediff server listening on: http://localhost:" + port);
 });
 
 function getDiff(a, b) {
-  return new Promise(function(resolve) {
+  return new Promise((resolve, reject) => {
     (new bd({
       imageAPath: imgPath(a),
       imageBPath: imgPath(b),
       imageOutputPath: imgPath(a + "-" + b),
-      threshold: 0,
+      outputBackgroundBlue: 255,
+      outputBackgroundGreen: 255,
+      outputBackgroundOpacity: .85,
+      outputBackgroundRed: 255,
       composition: false,
       delta: 0,
-      outputBackgroundRed: 255,
-      outputBackgroundGreen: 255,
-      outputBackgroundBlue: 255,
-      outputBackgroundOpacity: .85,
-    })).run(function(_, res) {
+      threshold: 0,
+    })).run((err, res) => {
+      if (err) return reject(err);
       resolve(Math.round((res.differences / res.dimension) * 1e4) / 1e2);
     });
   });
