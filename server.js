aeeb9ce03b691efae4871b60e6dd513c2c4e3895
silverwind <me@silverwind.io>
2016-05-17 19:13:27 +0200

    switch to blink-diff module

--- server.js
+++ server.js
@@ -6,18 +6,11 @@ const join = require("path").join;
 const express = require("express");
 const app = express();
 const Pageres = require("pageres");
-const resemble = require("node-resemble-js");
+const bd = require("blink-diff");
 const slugify = require("slugify-url");
 
 const client = join(__dirname, "client");
 const img = join(__dirname, "img");
-
-resemble.outputSettings({
-  errorColor: {red: 255, green: 64, blue: 64},
-  errorType: "movement",
-  transparency: 0.2,
-  largeImageThreshold: 0
-});
 
 function imgPath(name) { return join(img, slugify(name)) + ".png"; }
 function imgUrl(name) { return "img/" + slugify(name) + ".png"; }
@@ -29,16 +22,16 @@ app.use("/img", express.static(img, {maxAge: 0}));
 app.post("/diff", function(req, res) {
   var a = req.body.a, b = req.body.b;
   var dims = req.body.w + "x" + req.body.h;
-  new Pageres({delay: 2, crop: true, filename: "<%= url %>"})
+  new Pageres({delay: 1, crop: false, filename: "<%= url %>"})
   .src(a, [dims]).src(b, [dims]).dest(img).run()
   .then(function() {
-    getDiff(a, b).then(function(diff) {
+    getDiff(a, b).then(function(perc) {
       res.json({
         err: null,
         a: imgUrl(a),
         b: imgUrl(b),
         diff: imgUrl(a + "-" + b),
-        perc: diff.perc,
+        perc: perc,
       });
     });
   }).catch(function(err) {
@@ -54,13 +47,19 @@ app.listen(port, "0.0.0.0", function() {
 
 function getDiff(a, b) {
   return new Promise(function(resolve) {
-    fs.readFile(imgPath(a), function(_, aData) {
-      fs.readFile(imgPath(b), function(_, bData) {
-        resemble(aData).compareTo(bData).ignoreAntialiasing().ignoreColors().onComplete(function(data) {
-          data.getDiffImage().pack().pipe(fs.createWriteStream(imgPath(a + "-" + b)));
-          resolve({perc: data.misMatchPercentage});
-        });
-      });
+    (new bd({
+      imageAPath: imgPath(a),
+      imageBPath: imgPath(b),
+      imageOutputPath: imgPath(a + "-" + b),
+      threshold: 0,
+      composition: false,
+      delta: 0,
+      outputBackgroundRed: 255,
+      outputBackgroundGreen: 255,
+      outputBackgroundBlue: 255,
+      outputBackgroundOpacity: .85,
+    })).run(function(_, res) {
+      resolve(Math.round((res.differences / res.dimension) * 1e4) / 1e2);
     });
   });
 }
